910a07f20018d8131e892ff415c705f695e08b3a
CarolinaCedro <yotechgirl@gmail.com>
2023-06-19 18:52:34 -0300

    fix: deletando arquivos não utilizados

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,58 +2,15 @@ import {RouterModule, Routes} from '@angular/router'
 import {ClientesComponent} from './clientes/clientes/clientes.component';
 import {RelatoriosComponent} from './relatorios/relatorios/relatorios.component';
 import {ConfigurationComponent} from './configuration/configuration/configuration.component';
-import {PedidosComponent} from './pedidos/pedidos/pedidos.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {TarefaComponent} from "./tarefa/tarefa.component";
 import {LoginComponent} from "./login/login.component";
-import {RouteComponent} from "./route/route.component";
-import {AppComponent} from "./app.component";
-import {NavigationComponent} from "./navigation/navigation/navigation.component";
 import {HomeComponent} from "./home/home.component";
 import {NavbarComponent} from "./navbar/navbar/navbar.component";
 import {ProdutosComponent} from "./produtos/produtos.component";
 
 const routes: Routes = [
 
-  //<a href="#">Clientes</a>
-  //<a href="#">Contatos</a>
-  //<a href="#">Pedidos</a>
-  //<a href="#">Relatorios</a>
-  //<a href="#">Configurações</a>
-
-  //home
-  // {
-  //   path: '',
-  //   component: LoginComponent
-  // },
-  // {
-  //   path: 'clientes',
-  //   component: ClientesComponent
-  // },
-  // {
-  //   path: 'tarefa',
-  //   component: TarefaComponent
-  // },
-  // {
-  //   path: 'relatorios',
-  //   component: RelatoriosComponent
-  // },
-  // {
-  //   path: 'configuration',
-  //   component: ConfigurationComponent
-  // },
-  // {
-  //   path: 'pedidos',
-  //   component: PedidosComponent
-  // },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent
-  // },
-  // {
-  //   path: 'route',
-  //   component: ClientesComponent
-  // }
 
   // Rota de login
   {
@@ -81,10 +38,6 @@ const routes: Routes = [
       {
         path: 'configuration',
         component: ConfigurationComponent
-      },
-      {
-        path: 'pedidos',
-        component: PedidosComponent
       },
       {
         path: 'produtos',
